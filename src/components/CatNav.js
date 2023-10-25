037cbb261da8a0d9b5c926b8d9553cf9836a4cb0
Sameeran Amarnath <sameeranamarnath@gmail.com>
2023-10-26 01:00:31 +0530

    initial commit

--- src/components/CatNav.js
+++ src/components/CatNav.js
@@ -3,25 +3,22 @@ import "./_cat-nav.scss"
 const CatNav= function()
 {
 
+  let  categories=["Men","Women","Kids","Best Offers"];
     return(
-
+  
         <div className="cat-nav-container  ">
   <ul >
+  { categories.map((eachCategory,index)=>
+  {
+return (
   <li className="list-items "  >
-   <a href='#'> Men </a> 
+   <a href='#'> {eachCategory} </a> 
   </li>
-  <li className="list-items ">
-  <a href='#'> Women  </a>
-  </li>
-  <li className="list-items " >
-  <a href='#'>  Kids </a>
-  </li>
-  <li className="list-items " >
-  <a href='#'>  Best Offers </a>
-  </li>
-
+  )})}
+ 
 </ul>
         </div>
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -9,11 +9,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {configureStore} from "@reduxjs/toolkit";
+import { Provider } from 'react';
 
+const store= configureStore({
 
+  reducer:{
+
+      
+  }
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
     <App />
   </React.StrictMode>
 );
