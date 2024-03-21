d01c3ee3b092a0f98f7155576f96edfedda7d7d2
Sameeran Amarnath <sameeranamarnath@gmail.com>
2024-03-22 02:01:40 +0530

    Update CatNav.js

--- src/components/CatNav/CatNav.js
+++ src/components/CatNav/CatNav.js
@@ -6,7 +6,46 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 const CatNav= function()
 {
+import "./_cat-nav.scss"
+import {useDispatch,  useSelector} from "react-redux";
+import categorySlice from "../../Redux/Category/categorySlice";
+import { getCategories } from "../../Redux/Category/actions";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+const CatNav= function()
+{
 
+  let  categories= useSelector(state=>state.categoryReducer.categories);
+console.log(categories);
+
+ const dispatch= useDispatch();
+ 
+ useEffect(()=>{
+
+    dispatch(getCategories());
+
+ },[]);
+
+    return(
+  
+        <div className="cat-nav-container container ">
+  <ul >
+  <li className="list-items "  >
+   <Link to="/">Home</Link>
+   </li>
+  
+</ul>
+        </div>
+
+
+
+
+    )
+
+
+}
+
+export default CatNav
   let  categories= useSelector(state=>state.categoryReducer.categories);
 console.log(categories);
 
